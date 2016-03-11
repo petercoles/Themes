b8a5727a1ca99d3b1efa3232252d1c5af2768f45
Peter Coles <peter@kuroiweb.com>
2016-03-11 01:58:36 +0000

    Rename service provider and give it something useful to do

--- src/ServiceProvider.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Themes;
-
-class ServiceProvider
-{
-
-}
---
+++ src/ThemesServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Themes;
+
+use Illuminate\Support\ServiceProvider;
+
+class ThemesServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../config/themes.php' => config_path('themes.php'),
+        ]);
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app['view.finder'] = $this->app->share(function($app) {
+            $paths = $app['config']['view.paths'];
+
+            return new FileViewFinder($app['files'], $paths);
+        });
+    }
+}
