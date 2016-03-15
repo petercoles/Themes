c165534939acbe326f41de7fb22dcd90fd01ddcf
Peter Coles <peter@kuroiweb.com>
2016-03-15 11:43:55 +0000

    Additional test and tweak flow

--- tests/MatchMultipleTest.php
+++ tests/MatchMultipleTest.php
@@ -19,24 +19,37 @@ class MatchMultipleTest extends AbstractBaseTest
     {
         $config = [['match' => 'domain:www.test.tld|http_scheme:https', 'theme' => 'foo']];
         self::$config->shouldReceive('get')->with('themes')->andReturn($config);
-        self::$finder->shouldReceive('addLocation')->never();
         $this->request->shouldReceive('getHost')->andReturn('www.test.tld');
         $this->request->shouldReceive('getScheme')->andReturn('http');
+        self::$finder->shouldReceive('addLocation')->never();
         $this->themes->setTheme($this->request);
 
         $this->assertEquals(null, $this->themes->getTheme());
     }
 
-    public function testDomainMatched()
+    public function testDomainAndSchemeMatched()
     {
         $config = [['match' => 'domain:www.test.tld|http_scheme:https', 'theme' => 'foo']];
         self::$config->shouldReceive('get')->with('themes')->andReturn($config);
-        self::$finder->shouldReceive('addLocation')->once();
         $this->request->shouldReceive('getHost')->andReturn('www.test.tld');
         $this->request->shouldReceive('getScheme')->andReturn('https');
+        self::$finder->shouldReceive('addLocation')->once();
         $this->themes->setTheme($this->request);
 
         $this->assertEquals('foo', $this->themes->getTheme());
+    }
+
+    public function testTwoOutOfThreeNotMatched()
+    {
+        $config = [['match' => 'domain:www.test.tld|url_segment:admin|http_scheme:https', 'theme' => 'foo']];
+        self::$config->shouldReceive('get')->with('themes')->andReturn($config);
+        $this->request->shouldReceive('getHost')->andReturn('www.test.tld');
+        $this->request->shouldReceive('segment')->with(1)->andReturn('dashboard');
+        $this->request->shouldReceive('getScheme')->andReturn('https');
+        self::$finder->shouldReceive('addLocation')->never();
+        $this->themes->setTheme($this->request);
+
+        $this->assertEquals(null, $this->themes->getTheme());
     }
 
     public function tearDown()
